077af7c6009b4b45f9f00d7b72f018c88101d46a
Anay Goyal <anay.goyal@gmail.com>
2026-06-06 17:14:55 -0700

    Remove empty Wordle class definition

--- Wordle.java
+++ Wordle.java
@@ -1,4 +1,10 @@
-public class Wordle { 
-
-}
-
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
